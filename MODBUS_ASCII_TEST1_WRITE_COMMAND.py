b1c6efa5e60a283408533ad4b4abc6247e8cfefc
DAIKICHI-PSC <48910538+DAIKICHI-PSC@users.noreply.github.com>
2020-03-16 23:35:19 +0900

    Add files via upload

---
+++ MODBUS_ASCII_TEST1_WRITE_COMMAND.py
@@ -0,0 +1,46 @@
+#This script is to test MODBUS_ASCII_LRC with real serial communication
+
+import serial
+import time
+from MODBUS_ASCII_LRC import LRC_CREATE, LRC_CHECK
+
+COMPORT = "COM6"
+#COMMAND = "01039000000A"
+COMMAND = "02050403FF00"
+COM_TIMEOUT = 3
+
+client = serial.Serial(port=COMPORT, baudrate=230400, bytesize=8, parity='N', stopbits=1, timeout=1.0, xonxoff=0, rtscts=0, dsrdtr=None)
+print("[Serial status]")
+print(client)
+
+print("[Raw data to send]")
+print(COMMAND)
+ret, LRC_Command = LRC_CREATE(COMMAND) #Process data to send.
+print("[Status of LRC_CREATE]")
+print(ret)
+print("[Processed data by LRC_CREATE]")
+print(LRC_Command)
+client.write(bytes(LRC_Command, 'utf-8'))
+
+start_Time = time.time()
+LRC_Command = ""
+while(True):
+    serial_Buffer =client.readline()
+    LRC_Command = LRC_Command + serial_Buffer.decode("utf-8")
+    if ("\n" in LRC_Command) == True: #Check the charactors means the end of communication.
+        print("[Received raw data]")
+        print(LRC_Command)
+        ret, command = LRC_CHECK(LRC_Command) #Process received data.
+        print("[Status of LRC_CHECK]")
+        print(ret)
+        print("[Processed data by LRC_CHECK]")
+        print(command)
+        break
+    elapsed_Time = time.time() - start_Time
+    if elapsed_Time > COM_TIMEOUT: #Check elapsed time of communication.
+        print(">Connection time out.")
+        break
+
+client.close()
+print("[Serial status]")
+print(client)
